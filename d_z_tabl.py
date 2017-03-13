494ff5116154c3c1c3f5dab8b0b05c16da44e09d
RomaKubachi <jakka@list.ru>
2017-03-13 18:42:23 +0300

    Add files via upload

---
+++ d_z_tabl.py
@@ -0,0 +1,9 @@
+import random
+a = random.randint(1,10)
+b = random.randint(1,10)
+print(a,"*",b,"=")
+c = int(input ())
+if c== a * b:
+	print("верный ответ")
+else:
+	print("учи таблицу, двоечник!!!")
